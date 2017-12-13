fffe6329a0f34362f77f208b216f820bd65c0ff6
RSilviu <1silviu.rusu@gmail.com>
2017-12-13 04:31:54 +0200

    first angular attempt

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -3,16 +3,31 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TramsComponent } from './trams/trams.component';
 
+import { FormsModule } from '@angular/forms';
+import { TramDetailComponent } from './tram-detail/tram-detail.component';
+
+import { TramService } from "./tram.service";
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
+import { AppRoutingModule } from './/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TramsComponent,
+    TramDetailComponent,
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [TramService, MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
